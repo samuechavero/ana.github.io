0857af28361f696b7812699397bc22da2d54b8d8
Samuel  Chavero <samuchavero@Samuels-MacBook-Pro.local>
2026-03-22 02:58:21 -0500

    Fix router to hash and add test h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
@@ -18,7 +19,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <WouterRouter hook={useHashLocation}>
         <Router />
       </WouterRouter>
       <Toaster />
